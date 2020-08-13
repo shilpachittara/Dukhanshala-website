d3071551f8e9ebbe4a7c7ab708e34e1cb1b82358
shilpachittara <shilpachittara08@gmail.com>
2020-08-13 13:24:21 +0000

    certificate gcp

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ module.exports = {
 
     devServer:{
         https: true,
-        key: fs.readFileSync(""),
-        cert: fs.readFileSync(""),
-        ca: fs.readFileSync("")
+        key: fs.readFileSync("/etc/ssl/nopass.key"),
+        cert: fs.readFileSync("/etc/ssl/certificate.crt"),
+        ca: fs.readFileSync("/etc/ssl/ca_bundle.crt")
     }
 }
