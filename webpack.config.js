3522124312b757069b7373fdaa15a2dd8431f60f
shilpa chittara <shilpachittara@shilpas-MacBook-Pro.local>
2020-08-13 18:53:13 +0530

    webpack local

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+var fs = require('fs');
 module.exports = {
 
     devServer:{
         https: true,
-        key: fs.readFileSync("/etc/ssl/nopass.key"),
-        cert: fs.readFileSync("/etc/ssl/certificate.crt"),
-        ca: fs.readFileSync("/etc/ssl/ca_bundle.crt")
+        key: fs.readFileSync(""),
+        cert: fs.readFileSync(""),
+        ca: fs.readFileSync("")
     }
 }
