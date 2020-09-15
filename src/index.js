37567de24b753e978a81ae5d3b9fc6b46cf6f46a
shilpa chittara <shilpachittara@shilpas-MacBook-Pro.local>
2020-09-16 04:04:51 +0530

    website route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NewRoutes from './NewRoutes';
+import Routes from './Routes';
 import "./dukhanshala/assets/css/styles.css"
 // import Routes from 'Routes';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Routes/>
     <NewRoutes/>
   </React.StrictMode>,
   document.getElementById('root')
