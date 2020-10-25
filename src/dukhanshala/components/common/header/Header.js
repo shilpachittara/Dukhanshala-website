befd0bdc618264093bc106e4c9e509ba4481a59d
Anurag Shanu <s.anurag321@rediffmail.com>
2020-10-25 21:16:59 +0530

    search product implementation done

--- src/dukhanshala/components/common/header/Header.js
+++ src/dukhanshala/components/common/header/Header.js
@@ -6,6 +6,19 @@ import Search from '../search/Search';
 import { Link } from 'react-router-dom';
 import { AppContext } from 'Context/AppContext';
 import * as HeaderServices from '../../../../services/HeaderServices'
+import TextField from '@material-ui/core/TextField';
+import * as SearchServices from '../../../../services/SearchServices'
+import MenuItem from '@material-ui/core/MenuItem';
+import { Redirect } from "react-router-dom";
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import { Divider } from '@material-ui/core';
+
+
 
 class Header extends React.Component {
     static contextType = AppContext;
@@ -18,11 +31,14 @@ class Header extends React.Component {
             storeImage: logo,
             totalReferral: null,
             poweredBy: "Powered by",
-            dukaanshalaLogo: true
+            dukaanshalaLogo: true,
+            options: [],
+            storeId: "",
+            prodId:null
         }
     }
 
-    getStoreCode=()=>{
+    getStoreCode = () => {
         var path;
         if (this.context.storeCode != null) {
             path = this.context.storeCode;
@@ -58,13 +74,13 @@ class Header extends React.Component {
     }
 
     componentDidMount() {
-       this.getStoreCode()
+        this.getStoreCode()
     }
 
-    getStoreDetail = async(val) => {
-        let response =await HeaderServices.getStoreInfo(val);
+    getStoreDetail = async (val) => {
+        let response = await HeaderServices.getStoreInfo(val);
         try {
-            if (response &&response.status===200 && response.data ) {
+            if (response && response.status === 200 && response.data) {
                 this.setState({
                     storeName: response.data.storeName,
                     mobile: response.data.mobile
@@ -93,9 +109,50 @@ class Header extends React.Component {
 
 
     };
+
+    searchProduct = async (e) => {
+        let data = {}
+        data.storeCode = this.context.storeCode
+        data.val = e.target.value
+        if (e.target.value.length > 1) {
+            try {
+                let response = await SearchServices.getProductListBySearch(data)
+
+                if (response.data.products) {
+
+
+                    this.setState({ options: response.data.products })
+                }
+            }
+            catch (e) { alert(e) }
+        }
+
+
+
+    }
+
+    productDetail = (catName, catId, prodId) => {
+        this.context.updateCategoryName(catName);
+        this.context.updateCategoryId(catId);
+        this.context.updateProductId(prodId);
+        this.setState({prodId:prodId})
+        this.setState({redirect:true})
+     
+    }
+  
+
+
     render() {
+        const { redirect } = this.state
+
+        if (redirect)
+            return (<Redirect to={{
+                pathname:`${this.context.storeCode + "/product/detail/" + this.state.prodId}`
+            }} />)
+
         return (
             <div className="sticky-top headerPanel">
+           
                 <div className="container">
                     <div className="row align-items-center" style={{ minHeight: "90px" }}>
                         <div className="col-auto">
@@ -110,9 +167,48 @@ class Header extends React.Component {
 
                             </div>
                         </div>
-            
+
                     </div>
-                    <Search />
+                    {/* <Search /> */}
+
+                    {/* ============ */}
+                    <div>
+                        <TextField
+                            label="Search Product"
+                            id="outlined-size-small"
+                            variant="outlined"
+                            size="small"
+                            style={{ width: '100%' }}
+                            onChange={this.searchProduct}
+                        />
+                        {this.state.options.length > 0 &&
+                            <div className="option-search">
+                                {
+                                    this.state.options.map((option, index) =>
+                                    <div key={index} >
+
+                                    <List>
+                                        <ListItem style={{height:'50px'}}>
+                                            <ListItemAvatar>
+                                            
+                                                   <img src={option.productImage} alt={option.categoryName} style={{width:'40px',height:'40px',borderRadius:'8px'}}/>
+  
+                                            </ListItemAvatar>
+                                            <ListItemText onClick={() => { this.productDetail(option.categoryName, option.categoryId, option.productId) }} key={index}  primary={option.productName} secondary={option.sellingPrice} />
+                                        </ListItem>
+                                      
+                                    </List>
+                                    <Divider/>
+                                  
+                                    </div>
+                                        // <MenuItem onClick={() => { this.productDetail(option.categoryName, option.categoryId, option.productId) }} key={index} style={{ zIndex: 10, fontWeight: 'bold' }}>{option.productName}</MenuItem>
+
+                                    )
+                                }
+                            </div>
+                        }
+                    </div>
+                    {/* ============ */}
                 </div>
             </div>
         )
--- src/dukhanshala/components/common/search/Search.js
+++ src/dukhanshala/components/common/search/Search.js
@@ -1,12 +1,108 @@
-import React from 'react';
-import './Search.css';
-import searchImg from "../../../assets/images/search.svg"
+import React, { PureComponent } from 'react'
+import TextField from '@material-ui/core/TextField';
+import Divider from '@material-ui/core/Divider';
+import * as SearchServices from '../../../../services/SearchServices'
+import MenuItem from '@material-ui/core/MenuItem';
 
-const Search = () =>(
-    <div className="search_input_container">
-        <input type="search" className="search_input form-control" placeholder="Search categories or products" />
-        <img className="search_input_img" src={searchImg} alt="search" />
-    </div>
-)
+export default class Search extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            options: [],
+            storeId: ""
+        }
+    }
 
-export default Search;
+
+
+
+    componentDidMount = () => {
+        this.getStoreCode()
+    }
+    getStoreCode = () => {
+        var path;
+        let url = window.location.pathname;
+
+        let index = url.search("/")
+        let lastIndexOf = url.indexOf("/", url.indexOf("/") + 1);
+
+        if (index !== -1) {
+            let urlLength;
+            if (lastIndexOf === -1) {
+                let token = url;
+                this.setState({ storeId: token })
+
+            }
+            else {
+                urlLength = lastIndexOf;
+                let token = url.slice(index, urlLength);
+                this.setState({ storeId: token })
+
+
+            }
+
+        }
+        else {
+            alert("try again")
+        }
+
+
+    }
+    searchProduct = async (e) => {
+        let data = {}
+        data.storeCode = this.context.storeCode
+        data.val = e.target.value
+        if (e.target.value.length > 1) {
+            try {
+                let response = await SearchServices.getProductListBySearch(data)
+
+                if (response.data.products) {
+
+
+                    this.setState({ options: response.data.products })
+                    console.log(response.data.products)
+                }
+            }
+            catch (e) { alert(e) }
+        }
+
+
+
+    }
+
+    productDetail = (catName, catId, prodId) => {
+        console.log(catName, catId, prodId, "ccc")
+        this.context.updateCategoryName(catName);
+        this.context.updateCategoryId(catId);
+        this.context.updateProductId(prodId);
+        console.log("xx", this.state.storeId, prodId)
+        this.props.history.push(`${this.context.storeCode + "/product/detail/" + prodId}`);
+    }
+
+    render() {
+        return (
+            <div>
+                <TextField
+                    label="Search Product"
+                    id="outlined-size-small"
+                    variant="outlined"
+                    size="small"
+                    style={{ width: '100%' }}
+                    onChange={this.searchProduct}
+                />
+                {this.state.options.length > 0 &&
+                    <div className="option-search">
+                        {
+                            this.state.options.map((option, index) =>
+                               
+                               <div>
+                                    <MenuItem onClick={() => { this.productDetail(option.categoryName, option.categoryId, option.productId) }} key={index} style={{ zIndex: 10, fontWeight: 'bold' }}>{option.productName}</MenuItem>
+                                   
+                                </div>)
+                        }
+                    </div>
+                }
+            </div>
+        )
+    }
+}
